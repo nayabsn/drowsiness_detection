5c15c3c7c8ad118cf2afd0901bee9b816e7a6798
nayabsn <60349507+nayabsn@users.noreply.github.com>
2021-06-18 14:06:54 +0400

    Update and rename EAR.py to ear_detection.py

--- visual_detection/ear_detection.py
+++ visual_detection/ear_detection.py
@@ -1,7 +1,3 @@
-# USAGE
-# python pi_detect_drowsiness.py --cascade haarcascade_frontalface_default.xml --shape-predictor shape_predictor_68_face_landmarks.dat
-# python pi_detect_drowsiness.py --cascade haarcascade_frontalface_default.xml --shape-predictor shape_predictor_68_face_landmarks.dat --alarm 1
-
 # import the necessary packages
 from imutils.video import VideoStream
 from imutils import face_utils
