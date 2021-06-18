f020d2b90ad241c893e5a8c5cb9e71dda8e4259d
nayabsn <60349507+nayabsn@users.noreply.github.com>
2021-06-18 14:06:26 +0400

    detect_drowsiness_v1
    Version 1

--- visual_detection/detect_drowsiness.py
+++ visual_detection/detect_drowsiness.py
@@ -1,5 +1,4 @@
 # USAGE
-# python detect_drowsiness.py --shape-predictor shape_predictor_68_face_landmarks.dat
 # python detect_drowsiness.py --shape-predictor shape_predictor_68_face_landmarks.dat --alarm alarm.wav
 
 # --------------------------------------------------------
