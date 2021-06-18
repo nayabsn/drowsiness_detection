bacf38791ef09507050f643a08990c92d789c39d
nayabsn <60349507+nayabsn@users.noreply.github.com>
2021-06-18 09:47:42 +0100

    Update buzzer_test.py

--- alerting_system/buzzer_test.py
+++ alerting_system/buzzer_test.py
@@ -1,1 +1,14 @@
+#!/usr/python/python3
 
+from gpiozero import Buzzer
+from time import sleep
+
+buzzer=Buzzer(17)
+
+while True:
+    buzzer.on()
+    print("Buzz")
+    sleep(1)
+    buzzer.off()
+    print("------")
+    sleep(1)
